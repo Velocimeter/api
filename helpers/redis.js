586d11fe803e1a78250986eb8d29c4e876c7c8f6
torbik <liker1011@gmail.com>
2023-01-16 17:51:04 +0700

    redis logs

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,34 +1,43 @@
-const config = require('../config')
-const redis = require('redis');
+const config = require("../config");
+const redis = require("redis");
 
 const RedisClient = redis.createClient({
-  url: process.env.REDIS_URL
-})
+  url: process.env.REDIS_URL,
+});
 
 const connect = async () => {
   try {
-
-    let connected = false
+    let connected = false;
     try {
-      const pong = await RedisClient.ping()
-      if(pong == 'PONG') {
-        connected = true
+      const pong = await RedisClient.ping();
+      if (pong == "PONG") {
+        connected = true;
       }
-    } catch(ex) {
-      console.log(ex)
+    } catch (ex) {
+      console.log(ex);
+      console.warn(
+        "you got here because redis is closed, but i will open it next lines"
+      );
     }
 
-    if(!connected) {
+    if (!connected) {
       // TODO: Make it work without a running server
       await RedisClient.connect();
+
+      const pong = await RedisClient.ping();
+      if (pong == "PONG") {
+        console.log("all fine, connected");
+      } else {
+        console.log("it doesnt work");
+      }
     }
 
-    return RedisClient
+    return RedisClient;
   } catch (ex) {
-    console.log(ex)
+    console.log(ex);
   }
-}
+};
 
 module.exports = {
-  connect
-}
+  connect,
+};
