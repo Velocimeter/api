6d59b545eaea10f6c2277bc22ea28d73b45bffc4
torbik <liker1011@gmail.com>
2023-01-19 16:46:03 +0700

    Disable hardcode rewardRate

--- models/model.js
+++ models/model.js
@@ -427,24 +427,24 @@ const model = {
                   .rewards(idx)
                   .call()
                 const token = await model._getBaseAsset(web3, tokenAddress)
-                // let rewardRate = await bribeContract.methods
-                //   .rewardRate(tokenAddress)
-                //   .call()
+                const rewardRate = await bribeContract.methods
+                  .rewardRate(tokenAddress)
+                  .call()
 
                 // DUNKS THIS IS HARDCODED ONLY FOR PAIR CREATED WITH FRONTEND
                 // let rewardRate
-                if (
-                  // bribeAddress.toLowerCase() ===
-                  // '0xd205bE3baf65b642960E30747ba9ee36e8De2726'.toLowerCase()
-                  1 + 1 ===
-                  2
-                ) {
-                  rewardRate = 100
-                } else {
-                  rewardRate = await bribeContract.methods
-                    .rewardRate(tokenAddress)
-                    .call()
-                }
+                // if (
+                //   // bribeAddress.toLowerCase() ===
+                //   // '0xd205bE3baf65b642960E30747ba9ee36e8De2726'.toLowerCase()
+                //   1 + 1 ===
+                //   2
+                // ) {
+                //   rewardRate = 100
+                // } else {
+                //   rewardRate = await bribeContract.methods
+                //     .rewardRate(tokenAddress)
+                //     .call()
+                // }
                 return {
                   token: token,
                   rewardRate: BigNumber(rewardRate)
