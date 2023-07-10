10298b4b63612d81d2f6b5bb690c5110112ded45
t0rbik <liker1011@gmail.com>
2023-07-10 16:15:27 +0700

    json dumps aprs list

--- app/pairs/__init__.py
+++ app/pairs/__init__.py
@@ -31,7 +31,7 @@ class Pairs(object):
                 gauge = Gauge.find(pair.gauge_address)
                 data['gauge'] = gauge._data
                 data['gauge']['bribes'] = []
-                data['aprs'] = pair.aprs
+                data['aprs'] = json.dumps(pair.aprs)
                 for (token_addr, reward_ammount) in gauge.rewards:
                     data['gauge']['bribes'].append(
                         dict(
